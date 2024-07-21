a40351e6d2da254593d36a2c81d1184421aa375a
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-07-22 00:35:18 +0530

    Set user id instead of email in request from AuthMiddleware

--- backend/Middleware/AuthMiddleware.js
+++ backend/Middleware/AuthMiddleware.js
@@ -10,7 +10,7 @@ async function AuthMiddleware(req, res, next) {
     console.log(decoded);
     const user = await User.findOne({ email: decoded.email, password: decoded.password });
     if (!user) return res.status(401).json({ message: "User not found" });
-    req.email = user.email;
+    req.userId = user._id;
   } catch (err) {
     return res.status(401).json({ message: "Invalid token" });
   }
--- backend/Routes/UserActions/GetDetailsOfUser.js
+++ backend/Routes/UserActions/GetDetailsOfUser.js
@@ -4,9 +4,9 @@ const User = require("../../schema/UserSchema");
 const Accounts = require("../../schema/AccountsSchema");
 
 router.get("/", async (req, res) => {
-  const userEmail = req.email;
-  const user = await User.findOne({ email: userEmail });
-  const account = await Accounts.findOne({ user: user._id });
+  const userId = req.userId;
+  const user = await User.findOne({ _id: userId });
+  const account = await Accounts.findOne({ user: userId });
   res.status(200).json({ name: user.firstName + " " + user.lastName, email: user.email, balance: account.balance });
 });
 

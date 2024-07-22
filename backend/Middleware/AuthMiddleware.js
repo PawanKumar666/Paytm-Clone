b4e23b6528bffbea88b62adecb929001f3e18272
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-07-22 23:41:38 +0530

    Remove console log

--- backend/Middleware/AuthMiddleware.js
+++ backend/Middleware/AuthMiddleware.js
@@ -7,7 +7,6 @@ async function AuthMiddleware(req, res, next) {
   if (!token) return res.status(401).json({ message: "Token not found" });
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
-    console.log(decoded);
     const user = await User.findOne({ email: decoded.email, password: decoded.password });
     if (!user) return res.status(401).json({ message: "User not found" });
     req.userId = user._id;

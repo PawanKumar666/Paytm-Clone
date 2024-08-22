d7abbaa1a69180afc7b826bbfea1c3085b6ee168
PawanKumar666 <ssp.pawankumar001@gmail.com>
2024-08-22 22:05:44 +0530

    Formatting

--- frontend/src/components/Core/Button.jsx
+++ frontend/src/components/Core/Button.jsx
@@ -2,4 +2,4 @@ export default function Button({title, onClick}){
     return (
         <button type="button" className="bg-blue-500 text-white px-4 py-2 rounded-md" onClick={onClick}>{title}</button>
     )
-}
+}   
